953514bfeec148963350fa1f9b299188290fd4aa
AnantBhardwaj0909 <150821173+AnantBhardwaj0909@users.noreply.github.com>
2025-11-26 22:33:05 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
 import Image from 'next/image';
-import { FileText, GraduationCap, LayoutDashboard, PenBox } from 'lucide-react';
+import { FileText, GraduationCap, LayoutDashboard, PenBox, Camera } from 'lucide-react';
 import { Button } from './ui/button';
 import {DropdownMenu,DropdownMenuTrigger,DropdownMenuItem,DropdownMenuContent} from './ui/dropdown-menu'
 import { StarsIcon,ChevronDown} from 'lucide-react';
@@ -31,51 +31,70 @@ const Header =async() => {
                 <span className='hidden md:block'>Industry Insight</span>
               </Button>
             </Link>
+
           <DropdownMenu>
-          <DropdownMenuTrigger>              
-            <Button variant="outline" className="flex items-center gap-2">
+            <DropdownMenuTrigger>
+              <Button variant="outline" className="flex items-center gap-2">
                 <StarsIcon className="h-4 w-4" />
                 <span className='hidden md:block'>Growth Tools</span>
                 <ChevronDown className="h-4 w-4" />
               </Button>
             </DropdownMenuTrigger>
+
           <DropdownMenuContent>
             <DropdownMenuItem>
               <Link href={'/resume'} className='flex items-center gap-2'>
-              <FileText className="h-4 w-4" />
+                <FileText className="h-4 w-4" />
                 <span>Build Resume</span>
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>              
+
+            <DropdownMenuItem>
               <Link href={'/Cover'} className='flex items-center gap-2'>
-              <PenBox className="h-4 w-4" />
+                <PenBox className="h-4 w-4" />
                 <span>Cover Letter</span>
-              </Link></DropdownMenuItem>
-            <DropdownMenuItem>              
+              </Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem>
               <Link href={'/Interview'} className='flex items-center gap-2'>
-              <GraduationCap className="h-4 w-4" />
+                <GraduationCap className="h-4 w-4" />
                 <span>Interview Prep</span>
-              </Link></DropdownMenuItem>
+              </Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem>
+              <Link href={'/mock1'} className='flex items-center gap-2'>
+                <Camera className="h-4 w-4" />
+                <span>Mock Interviewer</span>
+              </Link>
+            </DropdownMenuItem>
+
           </DropdownMenuContent>
         </DropdownMenu>
-        <UserButton appearance={{
-          elements:{
-            avatarBox:'w-10 h-10',
-            userButtonPopoverCard:"shadow-xl",
-            userPreviewMainIdentifier:"font-semibold",
-          },
-        }}
-        afterSignOutUrl='/'
+
+        <UserButton 
+          appearance={{
+            elements:{
+              avatarBox:'w-10 h-10',
+              userButtonPopoverCard:"shadow-xl",
+              userPreviewMainIdentifier:"font-semibold",
+            },
+          }}
+          afterSignOutUrl='/'
         />
+
         </SignedIn>
-          <SignedOut>
-            <div className="flex gap-2">
-              <SignInButton>
-                <Button variant='Outline'>Sign In</Button>
-              </SignInButton>
-              <SignUpButton />
-            </div>
-          </SignedOut>
+
+        <SignedOut>
+          <div className="flex gap-2">
+            <SignInButton>
+              <Button variant='Outline'>Sign In</Button>
+            </SignInButton>
+            <SignUpButton />
+          </div>
+        </SignedOut>
+
         </div>
       </nav>
     </header>
