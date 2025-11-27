c418979db4c4efb41c77c1088b84137fd76d6296
AnantBhardwaj0909 <150821173+AnantBhardwaj0909@users.noreply.github.com>
2025-11-27 10:12:39 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -14,28 +14,34 @@ export default function Home() {
       <div className="grid-background"></div>
       <HeroSection />
       <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
-      <div className="container mx-auto px-4 md:px-6">
-        <h2 className="text-4xl font-extrabold tracking-tight text-center mb-16 text-gray-900 dark:text-white">Unleash the Power of Guru</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
-          {features.map((feature, index) => (
-            <Card
-              key={index}
-              className="border-2 border-transparent hover:border-primary shadow-sm hover:shadow-md transition-all duration-300 rounded-4xl backdrop-blur-md bg-opacity-90 ring-1 ring-green-400/20
-              "
-            >
-              <CardContent className="pt-6 pb-4 px-2 text-center flex flex-col items-center justify-start space-y-4">
-                <div className="text-4xl text-primary">{feature.icon}</div>
-                <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                  {feature.title}
-                </h3>
-                <p className="text-sm text-muted-foreground max-w-xs">
-                  {feature.description}
-                </p>
-              </CardContent>
-            </Card>
-        ))}
-      </div>
-      </div>
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-4xl font-extrabold tracking-tight text-center mb-16 text-gray-900 dark:text-white">
+            User Reviews
+          </h2>
+      
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+            {testimonial.map((feature, index) => (
+              <Card
+                key={index}
+                className="border-2 border-transparent hover:border-primary shadow-sm hover:shadow-md transition-all duration-300 rounded-4xl backdrop-blur-md bg-opacity-90 ring-1 ring-green-400/20"
+              >
+                <CardContent className="flex flex-col items-center text-center space-y-4 py-6">
+                  <h3 className="text-xl font-bold text-gray-800 dark:text-white">
+                    {feature.author}
+                  </h3>
+      
+                  <h4 className="text-sm font-semibold text-muted-foreground">
+                    {feature.role} · {feature.company}
+                  </h4>
+      
+                  <p className="text-sm text-muted-foreground max-w-xs">
+                    {feature.quote}
+                  </p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
       </section>
       <section className="w-full py-12 md:py-24 bg-muted/50">
         <div className="container mx-auto px-4 md:px-6">
